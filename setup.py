79f28587d96e7566f3877057f83d4cfc1a860b64
e.willhardy <e.willhardy@b102546a-eb6c-11de-b362-3513cf04e977>
2010-04-22 23:05:58 +0000

    Tweaking setup.py.
    git-svn-id: http://django-adjax.googlecode.com/svn/trunk@22 b102546a-eb6c-11de-b362-3513cf04e977

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     name = "Adjax",
     version = "1.0",
     packages = find_packages(exclude=["website*", "tests*", "design*"]),
-    url = 'http://adjax.hardysoftware.com.au/',
     install_requires = ['django>=1.0'],
     author = "Will Hardy",
     author_email = "adjax@hardysoftware.com.au",
